0a310676655d3b6f18fb09bc1aee291b111934b5
Admin <Admin@DESKTOP-7T0EOMO>
2024-03-11 21:52:21 +0530

    Add project files.

---
+++ BlazorServer/Item.cs
@@ -0,0 +1,9 @@
+﻿namespace BlazorServer
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ BlazorServer/ItemServices.cs
@@ -0,0 +1,18 @@
+﻿namespace BlazorServer
+{
+    public class ItemServices
+    {
+        public async Task<List<Item>> GetItemsAsync()
+        {
+            
+            await Task.Delay(100); 
+
+            return new List<Item>
+        {
+            new Item { Id = 1, Title = "Buy groceries", IsCompleted = false },
+            new Item { Id = 2, Title = "Do laundry", IsCompleted = true },
+            new Item { Id = 3, Title = "Clean the house", IsCompleted = false }
+        };
+        }
+    }
+}
